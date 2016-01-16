c07e37ba28467ec520707f9e842544a6589ebea3
Shaun Howard <smh150@case.edu>
2016-01-16 15:56:02 -0500

    Initial project setup.

---
+++ text2web/helpers.py
@@ -0,0 +1,1 @@
+__author__ = 'Shaun Howard'
---
+++ text2web/text2web.py
@@ -0,0 +1,12 @@
+__author__ = 'Shaun Howard'
+
+
+def text2web(text):
+    """
+    Interprets the provided text as a web control command if possible.
+    A message is returned to the user in text to describe the status
+    of the operation.
+    :param text: the text to control the default web browser
+    :return: the text response of the web control service
+    """
+    return text
