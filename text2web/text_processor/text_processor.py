3d86c3d499e0fc2742487c99e38a16b163668fd2
Shaun Howard <smh150@case.edu>
2016-01-23 13:52:10 -0500

    added finding of similar words, put off bi and tri-gram word analyzers.

--- text2web/text_processor/text_processor.py
+++ text2web/text_processor/text_processor.py
@@ -1,69 +1,120 @@
 __author__ = 'shaun'
-import nltk
+from itertools import chain
 
+from nltk import pos_tag, sent_tokenize, word_tokenize
+from nltk.corpus import wordnet as wn
 from text2web.helpers import normalize_string
 
 SUCCESS = "The operation was successful."
 FAIL = "The operation was unsuccessful."
 
 
-def parse_web_commands(text):
+def tag_words(words_of_text):
     """
-    Parses the provided text into web commands that should be executed
-    in a specific order from an English sentence.
-    :param text: the input command text
-    :return: the controls list, which will be empty if in error
+    Tags the provided words of the text with parts of speech.
+    :param text: the text to tag the words of
+    :return: the tags of the words in a map (k=word, v=pos_tag)
     """
-    # tokenize text into sentences
-    sentences = nltk.tokenize.sent_tokenize(text)
+    tags = []
+    tag_tuple_list = pos_tag(words_of_text)
+    for tag_tuple in tag_tuple_list:
+        word = tag_tuple[0]
+        tag = tag_tuple[1]
+        tags.append(tag)
+    return tags
 
-    # parse commands from each sentence
-    for sentence in sentences:
-        # tokenize the sentence into a sequence of one or more feasible commands
 
-        pass
+def similar_words(word, meaning):
+    """
+    Find words similar to the given word with the
+    given meaning, tagged with POS by nltk.
+    """
+    word_w_meaning = word + meaning
+    word_synset = wn.synset(word_w_meaning)
+    print "definition of %s" % word
+    print word_synset.definition()
+    # gather synonyms from lemmas, hyponyms (sub-types) and hypernyms (super-types)
+    synonyms = word_synset.lemma_names() \
+        + list(chain(*[l.lemma_names for l in word_synset.hyponyms()])) \
+        + list(chain(*[l.lemma_names for l in word_synset.hypernyms()]))
+    print "synonyms: "
+    print ', '.join(synonyms)
+    return synonyms
 
-    return []
+
+def process(sentence, words, tags):
+    """
+
+    :param sentence:
+    :param words:
+    :param tags:
+    :return:
+    """
+    return ""
+
+
+def valid_web_jargon(sent):
+    # TODO
+    return True
 
 
 class TextProcessor():
-    bigram_belief_state = dict()
 
     def __init__(self, training_data_dir):
         with open(training_data_dir, 'r') as training_file:
             training_data = training_file.read()
         training_command_list = training_data.split('\n')
-        self.create_bigram_belief_state(training_command_list)
 
-    def create_bigram_belief_state(self, training_command_list):
-        for command in training_command_list:
-            bigrams = self.determine_bigrams(command)
-            for bigram in bigrams:
-                norm_bigram_key = normalize_string(bigram[0])
-                norm_bigram_value = normalize_string(bigram[1])
-                if len(self.bigram_belief_state[norm_bigram_key]) == 0:
-                    self.bigram_belief_state[norm_bigram_key] = dict()
-                    self.bigram_belief_state[norm_bigram_key][norm_bigram_value] = 0
-                self.bigram_belief_state[norm_bigram_key][norm_bigram_value] += 1
-        self.bigram_belief_state = self.normalize_nested_dict(self.bigram_belief_state)
+    def process_web_action_requests(self, text):
+        """
+        Parses the provided text into web text actions that will be converted into
+        web actions by the web text to action mapper. The order will be maintained.
+        :param text: the input command text
+        :return: the controls list, which will be empty if in error
+        """
+        # tokenize text into sentences
+        sentences = sent_tokenize(text)
+        words_of_sentences = [word_tokenize(sent) for sent in sentences]
+        tags_of_words_of_sentences = [tag_words(words) for words in words_of_sentences]
 
-    def determine_bigrams(self, command):
-        sent_tokenized_commands = nltk.tokenize.sent_tokenize(command)
-        word_tokenized_commands = nltk.tokenize.word_tokenize(command)
-        for i in range(0, len(word_tokenized_commands)):
-            word = word_tokenized_commands[i]
-            bigrams = []
-            # do both left and right sides of word
-            if 0 < i < len(word_tokenized_commands) - 1:
-                bigrams = [(word, word_tokenized_commands[i - 1]),
-                           (word, word_tokenized_commands[i + 1])]
-            # only do right side of word
-            elif i == 0:
-                bigrams = [(word, word_tokenized_commands[i + 1])]
-            # only do left side of word
-            elif i == len(word_tokenized_commands) - 1:
-                bigrams = [(word, word_tokenized_commands[i - 1])]
-        return bigrams
+        # TODO: validate jargon
+        is_valid = [valid_web_jargon(sent) for sent in sentences]
+        for i in range(len(is_valid)):
+            if is_valid[i]:
+                # process sentence for commands
+                process(sentences[i], words_of_sentences[i], tags_of_words_of_sentences[i])
+        return []
+
+
+    # def create_bigram_belief_state(self, training_command_list):
+    #     for command in training_command_list:
+    #         bigrams = self.determine_bigrams(command)
+    #         for bigram in bigrams:
+    #             norm_bigram_key = normalize_string(bigram[0])
+    #             norm_bigram_value = normalize_string(bigram[1])
+    #             if len(self.bigram_belief_state[norm_bigram_key]) == 0:
+    #                 self.bigram_belief_state[norm_bigram_key] = dict()
+    #                 self.bigram_belief_state[norm_bigram_key][norm_bigram_value] = 0
+    #             self.bigram_belief_state[norm_bigram_key][norm_bigram_value] += 1
+    #     self.bigram_belief_state = self.normalize_nested_dict(self.bigram_belief_state)
+    #
+    # def determine_bigrams(self, command):
+    #     sent_tokenized_commands = sent_tokenize(command)
+    #     word_tokenized_commands = word_tokenize(command)
+    #     for i in range(0, len(word_tokenized_commands)):
+    #         word = word_tokenized_commands[i]
+    #         bigrams = []
+    #         # do both left and right sides of word
+    #         if 0 < i < len(word_tokenized_commands) - 1:
+    #             bigrams = [(word, word_tokenized_commands[i - 1]),
+    #                        (word, word_tokenized_commands[i + 1])]
+    #         # only do right side of word
+    #         elif i == 0:
+    #             bigrams = [(word, word_tokenized_commands[i + 1])]
+    #         # only do left side of word
+    #         elif i == len(word_tokenized_commands) - 1:
+    #             bigrams = [(word, word_tokenized_commands[i - 1])]
+    #     return bigrams
 
     def normalize_nested_dict(self, dict_of_dict):
         new_dict_of_dict = dict()
