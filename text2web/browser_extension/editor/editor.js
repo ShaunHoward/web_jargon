7a4ebac496e52bad184684ae3361af2c60e938fa
Elliot Essman <et8109@gmail.com>
2016-01-27 00:19:41 -0500

    added scrolling functions

--- text2web/browser_extension/editor/editor.js
+++ text2web/browser_extension/editor/editor.js
@@ -3,7 +3,7 @@
  */
 function executeCommand(command){
   alert("executing "+command);
-  chrome.tabs.executeScript(null, { code: "appendToLinks();"});
+  chrome.tabs.executeScript(null, { code: command.func=";"/*"scrollUp();"*/});
 }
 
 function appendToLinks(){
@@ -12,4 +12,46 @@ function appendToLinks(){
 
 function createTab(){
   chrome.tabs.create({ url: "http://cnn.com" });
+}
+
+function scrollDown(){
+  var current = $(document).scrollTop();
+  var dest = current + $(window).height();
+  _scrollVertical(dest);
+}
+
+function scrollUp(){
+  var current = $(document).scrollTop();
+  var dest = current - $(window).height();
+  _scrollVertical(dest);
+}
+
+function scrollLeft(){
+  var current = $(document).scrollLeft();
+  var dest = current - $(window).width();
+  _scrollHorizontal(dest);
+}
+
+function scrollRight(){
+  var current = $(document).scrollLeft();
+  var dest = current + $(window).width();
+  _scrollHorizontal(dest);
+}
+
+
+function _scrollHorizontal(dest){
+  $('html, body').animate({
+    scrollLeft: dest
+  }, 1000);
+}
+
+function _scrollVertical(dest){
+  $('html, body').animate({
+    scrollTop: dest
+  }, 1000);
+}
+
+function _addBox(){
+  $('html').append('<div>heyoooooo</div>');
+  alert("done");
 }
