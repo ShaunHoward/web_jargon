d2bb785f05d8cff33909e991c874906fd4cb77c3
Shaun Howard <smh150@case.edu>
2016-02-12 04:00:34 -0500

    added requirements to templates

--- tests/test_web_jargon.py
+++ tests/test_web_jargon.py
@@ -1,6 +1,6 @@
 __author__ = 'shaun howard'
 import unittest
-from web_jargon import web_jargon as tw
+from web_jargon import web_jargon as wj
 
 
 class Text2WebText(unittest.TestCase):
@@ -9,14 +9,14 @@ class Text2WebText(unittest.TestCase):
         pass
 
     def test_scroll_up_text2web(self):
-        json_actions = tw.web_jargon("Scroll up in the current webpage.")
+        json_actions = wj.extract_web_actions("Scroll up in the current webpage.")
         self.assertGreater(len(json_actions), 0)
         lower_actions = json_actions.lower()
         self.assertTrue("scroll" in lower_actions)
         self.assertTrue("up" in lower_actions)
 
     def test_scroll_down_text2web(self):
-        json_actions = tw.web_jargon("Scroll down a page.")
+        json_actions = wj.extract_web_actions("Scroll down a page.")
         self.assertGreater(len(json_actions), 0)
         lower_actions = json_actions.lower()
         self.assertTrue("scroll" in lower_actions)
