468efbbc0df978f5891ea92724d369d79521781e
Elliot Essman <et8109@gmail.com>
2016-02-13 16:22:47 -0500

    reverted request sending

--- web_jargon/browser_extension/popup/popup.js
+++ web_jargon/browser_extension/popup/popup.js
@@ -4,15 +4,14 @@ var backgroundPage = chrome.extension.getBackgroundPage();
  * Interrupts extension button text input form to manually post to server
  */
 $("#textInput").submit(function( event ) {
-  backgroundPage.doCommand("createTab", ["cnn"]);
-  /*var server = "http://localhost:8080/";
+  var server = "http://localhost:8080/";
   var inputText = $("input:first").val();
   event.preventDefault();
   $.post( server, inputText, function( data ) {
   $("#JargonPopup").append("</br>"+data);
   var commands = JSON.parse(data)["actions"];
-  var msg = backgroundPage.doCommand(commands);
-  });*/
+  var msg = backgroundPage.doCommand(commands, params);
+  });
 });
 
 $("#listenButton").click(function( event ) {
